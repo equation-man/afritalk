fbc68a77a2402381f0a89bf71eb7e717703050d3
equation-man <bigtechguyz@gmail.com>
2025-06-09 20:38:03 +0300

    Stopped tracking target

--- target/debug/deps/encode_decode-4d30715308961167.d
+++
@@ -1,7 +0,0 @@
-/home/odin/Desktop/software_engineering_laboratory/hangouts/rust_systems/systems_programming/afritalk/target/debug/deps/encode_decode-4d30715308961167: encode_decode/src/lib.rs encode_decode/src/utils.rs encode_decode/src/encodec.rs
-
-/home/odin/Desktop/software_engineering_laboratory/hangouts/rust_systems/systems_programming/afritalk/target/debug/deps/encode_decode-4d30715308961167.d: encode_decode/src/lib.rs encode_decode/src/utils.rs encode_decode/src/encodec.rs
-
-encode_decode/src/lib.rs:
-encode_decode/src/utils.rs:
-encode_decode/src/encodec.rs:
--- target/debug/deps/main-0fd3ab371e97b507.d
+++
@@ -1,7 +0,0 @@
-/home/odin/Desktop/software_engineering_laboratory/hangouts/rust_systems/systems_programming/afritalk/target/debug/deps/libmain-0fd3ab371e97b507.rmeta: main/src/lib.rs
-
-/home/odin/Desktop/software_engineering_laboratory/hangouts/rust_systems/systems_programming/afritalk/target/debug/deps/libmain-0fd3ab371e97b507.rlib: main/src/lib.rs
-
-/home/odin/Desktop/software_engineering_laboratory/hangouts/rust_systems/systems_programming/afritalk/target/debug/deps/main-0fd3ab371e97b507.d: main/src/lib.rs
-
-main/src/lib.rs:
